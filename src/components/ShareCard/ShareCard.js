3d382ff018d4aae70b67489a9975a80d975f2f25
mindplates <mindplates@gmail.com>
2020-07-06 01:20:47 +0900

    공유 카드 아이콘 툴팁 추가

--- src/components/ShareCard/ShareCard.js
+++ src/components/ShareCard/ShareCard.js
@@ -3,15 +3,29 @@ import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import ReactTimeAgo from 'react-time-ago';
 import moment from 'moment';
+import ReactTooltip from 'react-tooltip';
 import { Button, Card, CardBody } from '@/components';
 import { SharePropTypes } from '@/proptypes';
 import './ShareCard.scss';
 
 class ShareCard extends React.PureComponent {
+  componentDidMount() {
+    ReactTooltip.rebuild();
+  }
+
+  componentDidUpdate() {
+    ReactTooltip.rebuild();
+  }
+
   render() {
     const { className, share, onConfigClick, onCardClick, t } = this.props;
-    const lastBucket = share.shareTimeBuckets && share.shareTimeBuckets.length > 0 ? share.shareTimeBuckets[share.shareTimeBuckets.length - 1] : {};
-    const joinedAdminUser = share.shareUsers.find((shareUser) => shareUser.shareRoleCode === 'ADMIN' && shareUser.status === 'ONLINE');
+    const lastBucket =
+      share.shareTimeBuckets && share.shareTimeBuckets.length > 0
+        ? share.shareTimeBuckets[share.shareTimeBuckets.length - 1]
+        : {};
+    const joinedAdminUser = share.shareUsers.find(
+      (shareUser) => shareUser.shareRoleCode === 'ADMIN' && shareUser.status === 'ONLINE',
+    );
     const totalUserCount = share.onLineUserCount + share.offLineUserCount;
 
     return (
@@ -29,14 +43,23 @@ class ShareCard extends React.PureComponent {
             <ReactTimeAgo date={moment(lastBucket.openDate).valueOf()} />
           </div>
           <div className="tag-info">
-            <span className={`short-icon admin-join ${joinedAdminUser ? 'joined' : ''}`}>
+            <span
+              data-tip={t('매니저가 참여중입니다')}
+              className={`short-icon admin-join ${joinedAdminUser ? 'joined' : ''}`}
+            >
               <i className="fal fa-head-side" />
             </span>
-            <span className={`short-icon status  ${share.startedYn ? 'started' : ''}`}>
+            <span
+              data-tip={share.startedYn ? t('공유 진행중') : t('공유 준비중')}
+              className={`short-icon status  ${share.startedYn ? 'started' : ''}`}
+            >
               {share.startedYn && <i className="fal fa-play" />}
               {!share.startedYn && <i className="fal fa-pause" />}
             </span>
-            <span className={`short-icon user-count-icon ${totalUserCount > 0 ? 'has-joined-user' : ''}`}>
+            <span
+              data-tip={`${totalUserCount}${t('명의 사용자가 참여중입니다.')}`}
+              className={`short-icon user-count-icon ${totalUserCount > 0 ? 'has-joined-user' : ''}`}
+            >
               <i className="fas fa-street-view" />
             </span>
             <span className="user-count">{totalUserCount}</span>
